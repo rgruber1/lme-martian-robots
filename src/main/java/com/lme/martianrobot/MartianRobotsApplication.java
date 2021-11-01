57d1a5263ee1481757c604fc3b6c722e63f6c203
Robert Gruber <gruber.robert@gmail.com>
2021-11-01 11:57:09 +0000

    Added lombok; renamed package.

---
+++ src/main/java/com/lme/martianrobot/MartianRobotsApplication.java
@@ -0,0 +1,16 @@
+package com.lme.martianrobot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class MartianRobotsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MartianRobotsApplication.class, args);
+        log.info("Application started");
+    }
+
+}
--- src/test/java/com/lme/martianrobot/MartianRobotsApplicationTests.java
+++ src/test/java/com/lme/martianrobot/MartianRobotsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.lme.martianrobots;
+package com.lme.martianrobot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
